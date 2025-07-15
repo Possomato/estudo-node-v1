3dea2b08ed82fc5e17dc7b72acd86426bfc87d2d
Possomato <gabrielb.possomato@gmail.com>
2025-07-15 18:15:05 -0300

    add - index, update and show on curses controllers

--- src/controller/CoursesController.ts
+++ src/controller/CoursesController.ts
@@ -9,4 +9,23 @@ export class CoursesController {
     res.status(201).json(name)
   }
 
+  async index(req: Request, res: Response) {
+    const courses = await knex('courses').select()
+
+    res.send(courses)
+  }
+
+  async show(req: Request, res: Response){
+    const {id} = req.params
+    const coursesFiltered = await knex('courses').where({id})
+    res.send(coursesFiltered)
+  }
+
+  async update(req: Request, res: Response) {
+    const { id } = req.params
+    const {name} = req.body
+
+    await knex('courses').update({ name }).where({ id })
+    res.json(name)
+  }
 }
--- src/routes/courses.routes.ts
+++ src/routes/courses.routes.ts
@@ -5,5 +5,8 @@ const coursesRouter = Router()
 const coursesController = new CoursesController()
 
 coursesRouter.post('/', coursesController.create)
+coursesRouter.get('/', coursesController.index)
+coursesRouter.put('/:id', coursesController.update)
+coursesRouter.get('/:id', coursesController.show)
 
 export {coursesRouter}
